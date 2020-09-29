364029e2b6148e1dcb0cea1585bf3204667f48cf
Hugo Claesen <hugo_claesen@hotmail.com>
2020-09-29 18:19:46 +0200

    Add project files.

---
+++ Roman.UnitTest/RomanTest.cs
@@ -0,0 +1,111 @@
+﻿using RomanNumerals;
+using System;
+using System.Net;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+namespace Roman.UnitTest
+{
+    public class RomanTest
+    {
+        [Fact]
+        public void GivenInteger1_ReturnI()
+        {
+            string romanNumber = 1.ToRoman();
+            Assert.Equal<string>("I", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void GivenInteger5_ReturnV()
+        {
+            string romanNumber = 5.ToRoman();
+            Assert.Equal<string>("V", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void GivenInteger10_ReturnX()
+        {
+            string romanNumber = 10.ToRoman();
+            Assert.Equal<string>("X", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void GivenInteger50_ReturnL()
+        {
+            string romanNumber = 50.ToRoman();
+            Assert.Equal<string>("L", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void GivenInteger100_ReturnC()
+        {
+            string romanNumber = 100.ToRoman();
+            Assert.Equal<string>("C", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void GivenInteger500_ReturnD()
+        {
+            string romanNumber = 500.ToRoman();
+            Assert.Equal<string>("D", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void GivenInteger1000_ReturnM()
+        {
+            string romanNumber = 1000.ToRoman();
+            Assert.Equal<string>("M", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void GivenInteger2_ReturnII()
+        {
+            string romanNumber = 2.ToRoman();
+            Assert.Equal<string>("II", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void GivenInteger4_ReturnIV()
+        {
+            string romanNumber = 4.ToRoman();
+            Assert.Equal<string>("IV", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void GivenInteger9_ReturnIX()
+        {
+            string romanNumber = 9.ToRoman();
+            Assert.Equal<string>("IX", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void GivenInteger40_ReturnXL()
+        {
+            string romanNumber = 40.ToRoman();
+            Assert.Equal<string>("XL", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void GivenInteger94_ReturnXCIV()
+        {
+            string romanNumber = 94.ToRoman();
+            Assert.Equal<string>("XCIV", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void GivenInteger9494_ReturnMMMMMMMMMCLXCIV()
+        {
+            string romanNumber = 9494.ToRoman();
+            Assert.Equal<string>("MMMMMMMMMCDXCIV", romanNumber, StringComparer.Ordinal);
+        }
+
+        [Fact]
+        public void CompareToThirdParty()
+        {
+            for (int i = 1; i < 500; ++i)
+            {
+                Assert.Equal(i.ToRoman(), i.ToRomanNumerals());
+            }
+        }
+    }
+}
---
+++ Roman/ExtensionMethods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace Roman
+{
+    public static class ExtensionMethods
+    {
+        public static string ToRoman(this int i)
+        {
+            int thousands = i / 1000;
+            int fivehundreds = (i - thousands * 1000) / 500;
+            int hundreds = (i - thousands * 1000 - fivehundreds * 500) / 100;
+            int fiftees = (i - thousands * 1000 - fivehundreds * 500 - hundreds * 100) / 50;
+            int tens = (i - thousands * 1000 - fivehundreds * 500 - hundreds * 100 - fiftees * 50) / 10;
+            int fives = (i - thousands * 1000 - fivehundreds * 500 - hundreds * 100 - fiftees * 50 - tens * 10) / 5;
+            int ones = (i - thousands * 1000 - fivehundreds * 500 - hundreds * 100 - fiftees * 50 - tens * 10 - fives * 5);
+
+            StringBuilder romanNumber = new StringBuilder();
+
+            romanNumber.Append('M', thousands);
+            AppendOrder(fivehundreds, 'D', hundreds, 'C', 'M', romanNumber);
+            AppendOrder(fiftees, 'L', tens, 'X', 'C', romanNumber);
+            AppendOrder(fives, 'V', ones, 'I', 'X', romanNumber);
+
+            return romanNumber.ToString();
+        }
+
+        private static void AppendOrder(int fives, char fiveSymbol, int ones, char oneSymbol, char tenSymbol, StringBuilder romanNumber)
+        {
+            romanNumber.Append(fiveSymbol, fives);
+
+            if (ones == 4 && fives == 1)
+            {
+                romanNumber.Remove(romanNumber.Length - 1, 1);
+                romanNumber.Append(string.Concat(oneSymbol, tenSymbol));
+            }
+            else if (ones == 4)
+            {
+                romanNumber.Append(string.Concat(oneSymbol, fiveSymbol));
+            }
+            else
+            {
+                romanNumber.Append(oneSymbol, ones);
+            }
+        }
+    }
+}
